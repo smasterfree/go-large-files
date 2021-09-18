03790e31ff26f304198ad6afb04aa16ebdb6ab41
hzhuangzhexiao <hzhuangzhexiao@corp.netease.com>
2021-09-18 10:28:14 +0800

    fix rev2 bug

--- rev2/readfile2.go
+++ rev2/readfile2.go
@@ -54,10 +54,17 @@ func main() {
 
 	linesChunkLen := 64 * 1024
 	lines := make([]string, 0, 0)
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
-		if len(lines) == linesChunkLen {
+	scanner.Scan()
+	for {
+		lines = append(lines, scanner.Text())
+		willScan := scanner.Scan()
+		if len(lines) == linesChunkLen || !willScan {
+
+			//WaitGroup 对象内部有一个计数器，最初从0开始，它有三个方法：Add(),
+			//Done(), Wait() 用来控制计数器的数量。Add(n) 把计数器设置为n ，
+			//Done() 每次把计数器-1 ，wait() 会阻塞代码的运行，直到计数器地值减
+			//为0。
+
 			wg.Add(len(lines))
 			process := lines
 			go func() {
@@ -79,6 +86,9 @@ func main() {
 				}
 			}()
 			lines = make([]string, 0, linesChunkLen)
+		}
+		if !willScan {
+			break
 		}
 	}
 	wg.Wait()
